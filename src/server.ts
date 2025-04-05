ef9f2b082b83305731197dc848311234e0c100db
Douglas <dhidekim@gmail.com>
2025-04-05 01:39:58 -0300

    Banco de dados - Realizando queries com Knex

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,27 @@
 import fastify from 'fastify'
+import cryto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+app.get('/insert', async () => {
+  const transactions = await knex('transactions')
+    .insert({
+      id: cryto.randomUUID(),
+      title: 'Transação teste',
+      amount: 1000,
+    })
+    .returning('*')
 
-  return tables
+  return transactions
+})
+
+app.get('/select', async () => {
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transactions
 })
 
 app
